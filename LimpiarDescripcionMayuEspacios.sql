2b58ce26e35140eb51b624e237f3c3e2eb141fe2
Eder Raul Perez Gallardo Hernandez <ederaul_@hotmail.com>
2025-02-24 18:04:30 -0600

    Update LimpiarDescripcionMayuEspacios24.02.25.sql

--- LimpiarDescripcionMayuEspacios.sql
+++ LimpiarDescripcionMayuEspacios.sql
@@ -1,5 +1,9 @@
 
 
+/*Esta consulta, crear la funciÃ³n donde limpia espacios y cambia el texto a mayÃºsculas de una columna llamada descripciones, 
+la finalidad es limpiar la columna de espacios de mÃ¡s o dobles, saltos de lineal y quitar acentos esto con la finalidad de poder 
+hacer una homologaciÃ³n de texto mÃ¡s necesaria para ciertos informes.*/
+
 
 
 /*Para utilizar esta funcion en otra base se tiene que nombrar la base y poner el go*/
@@ -15,33 +19,33 @@ RETURNS NVARCHAR(MAX)
 AS
 BEGIN
 
-	  -- Convertir a mayúsculas
+	  -- Convertir a mayÃºsculas
     SET @texto = UPPER(@texto);
 
 	-- Elimina acentos
 
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'á', 'a');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'é', 'e');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'í', 'i');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'ó', 'o');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'ú', 'u');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'ñ', 'n');
-	SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Á', 'A');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'É', 'E');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Í', 'I');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ó', 'O');
-    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ú', 'U');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã¡', 'a');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã©', 'e');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã­', 'i');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã³', 'o');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ãº', 'u');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã±', 'n');
+	SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã', 'A');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã‰', 'E');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã', 'I');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ã“', 'O');
+    SET @texto = REPLACE(@texto COLLATE Latin1_General_BIN, 'Ãš', 'U');
 
 	    
-    -- Eliminar puntuaciones y reemplazar múltiples espacios con uno solo
+    -- Eliminar puntuaciones y reemplazar mÃºltiples espacios con uno solo
     SET @texto = REPLACE(@texto, CHAR(13), ''); -- Eliminar retornos de carro
-    SET @texto = REPLACE(@texto, CHAR(10), ''); -- Eliminar saltos de línea
+    SET @texto = REPLACE(@texto, CHAR(10), ''); -- Eliminar saltos de lÃ­nea
 
 	-- Espacios al inicio y al final
 
 	  RETURN LTRIM(RTRIM(@texto));
 
-    -- Reemplazar múltiples espacios con uno solo
+    -- Reemplazar mÃºltiples espacios con uno solo
     WHILE CHARINDEX('  ', @texto) > 0
     BEGIN
         SET @texto = REPLACE(@texto, '  ', ' ');
